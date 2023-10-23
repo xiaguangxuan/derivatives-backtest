51ed86be9b53d44a86d3d8f54278c43c6ad473d0
WangJunyi <junyiwang123@126.com>
2023-10-23 14:29:12 +0800

    format data source and date-related functions

--- backtest_version_V2/getData.py
+++ backtest_version_V2/getData.py
@@ -1,54 +1,54 @@
 import pandas as pd
-import efinance as ef
-import akshare as ak
 from datetime import datetime
 
-underlying_dict = {"上证50": "000016.SH"}
+import efinance as ef
+import WindPy
+WindPy.w.start()
 
+underlying_dict = {
+    "上证50": "000016.SH",
+    "沪深300": "000300.SH",
+    "中证500": "000905.SH",
+    "中证1000": "000852.SH",
+}
 
-def fetching_data(underlying, index_data_source='wind',show_data_info=False):
-    if index_data_source == 'wind_file':
-        index_data = pd.read_excel('./data/%s.xlsx'%(underlying))
-        index_data = index_data.loc[:,['日期','收盘价(元)']]
-        index_data['收盘价(元)'] = index_data['收盘价(元)'].round(2)
-        correct_time_type = type(pd.to_datetime('1970-01-01 08:00:00')) # 正常的日历类型应该为correct_time_type
-        if_correct_time_type = [type(index_data.loc[i,'日期'])==correct_time_type for i in range(index_data.shape[0])] # 记录bool组
-        print('[Message from function: fetching_data] 即将清除无效日期数据————总共清除%d组'%(if_correct_time_type.count(False)))
-        index_data = index_data[if_correct_time_type] # mask运算，只保留True的位置
-        index_data.set_index('日期',drop=True,inplace=True)
-    
-    elif index_data_source == 'efinance':
-        index_data = ef.stock.get_quote_history(underlying)
-        index_data = index_data.loc[:,['日期','收盘']]
-        index_data.set_index('日期',drop=True,inplace=True)
-    
-    elif index_data_source == 'akshare':
-        index_data = ak.fund_etf_hist_sina(symbol=underlying) 
-        index_data = index_data.loc[:,['date','close']]
-        index_data.set_index('date',drop=True,inplace=True)
-        
-    if show_data_info:
-        print(index_data)
-        
-    return index_data
+root_path = "D:/10-国君/GTJA-intern-codes"
 
 
 def get_underlying_data(underlying, start_date, end_date, data_source="wind"):
-    '''underlying暂时为单个数据源，date为datetime格式'''
+    """
+    Args:
+        underlying (str): underlying name
+        start_date (datetime): start date
+        end_date (datetime): end date
+        data_source (str, optional): index data source. Defaults to "wind".
+    Returns:
+        df: underlying index price data
+    """
     if type(underlying) == str:
         if data_source == "wind":
-            import WindPy
-            WindPy.w.start()
-            index_data = WindPy.w.wsd(underlying_dict[underlying], 'CLOSE', start_date.strftime('%Y%m%d'), end_date.strftime('%Y%m%d'), "Fill=Previous", usedf=True)[1].dropna()
-            # index_data.index = index_data.index.map(lambda x: datetime.strptime(x, '%Y-%m-%d').date())
-            index_data.columns = ['close']
-            return index_data 
+            index_data = WindPy.w.wsd(
+                underlying_dict[underlying],
+                "CLOSE",
+                start_date.strftime("%Y%m%d"),
+                end_date.strftime("%Y%m%d"),
+                "Fill=Previous",
+                usedf=True,
+            )[1].dropna()
+            index_data.columns = ["close"]
+            return index_data
         elif data_source == "efinance":
-            index_data = ef.stock.get_quote_history(stock_codes = underlying, beg=start_date.strftime('%Y%m%d'), end=end_date.strftime('%Y%m%d'))
-            index_data = index_data.loc[:, ['日期','收盘']]
-            index_data.columns = ['date', 'close']
-            index_data = index_data.set_index('date', drop=True)
-            index_data.index = index_data.index.map(lambda x: datetime.strptime(x, '%Y-%m-%d').date())
+            index_data = ef.stock.get_quote_history(
+                stock_codes=underlying,
+                beg=start_date.strftime("%Y%m%d"),
+                end=end_date.strftime("%Y%m%d"),
+            )
+            index_data = index_data.loc[:, ["日期", "收盘"]]
+            index_data.columns = ["date", "close"]
+            index_data = index_data.set_index("date", drop=True)
+            index_data.index = index_data.index.map(
+                lambda x: datetime.strptime(x, "%Y-%m-%d").date()
+            )
             return index_data
         
     elif type(underlying) == list:
@@ -57,8 +57,19 @@ def get_underlying_data(underlying, start_date, end_date, data_source="wind"):
 
 
 def get_trading_datelist():
-    underlying = "上证50"
-    index_data_source = "efinance"
-    index_data = fetching_data(underlying, index_data_source,show_data_info=False)
-    trading_datelist = [pd.to_datetime(x).date() for x in index_data.index]
+    trading_date_df = pd.read_csv(f"{root_path}/backtest_version_V2/basic_data/trading_datelist.csv", index_col=0)
+    trading_date_df["date"] = trading_date_df["date"].apply(lambda x: pd.to_datetime(x).date())
+    trading_datelist = list(trading_date_df["date"])
     return trading_datelist
+
+
+def update_trading_datelist():
+    trading_datelist = WindPy.w.tdays("20100101", "20241231").Data[0]
+    trading_date_df = pd.DataFrame(trading_datelist)
+    trading_date_df.columns = ["date"]
+    trading_date_df.to_csv(f"{root_path}/backtest_version_V2/basic_data/trading_datelist.csv")
+    return None
+
+
+if __name__ == "__main__":
+    update_trading_datelist()
